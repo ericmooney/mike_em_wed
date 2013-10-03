a577f5c8d075dbb0e956d573e75796bc76e3c8b7
Eric Mooney <eric.c.mooney@gmail.com>
2013-10-02 20:43:46 -0700

    Initial site with bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MikeEmWed::Application.config.secret_token = 'c637ca4141b9d30471fd069d293784a2ae183a5aff62d28929dd052ae4d56f204bbb7e8a10cad422776fcbcf69bbd459a0b4121e23c7694f9099ee6e9475def0'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+MikeEmWed::Application.routes.draw do
+
+  get "home/index"
+
+  root :to => 'home#index'
+
+
+end
