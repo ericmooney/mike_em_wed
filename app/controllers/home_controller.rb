1646466372238621f981e649445aa530f0e98863
Eric Mooney <eric.c.mooney@gmail.com>
2013-10-12 12:00:04 -0700

    Basic pages ready for launch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,21 @@
 class HomeController < ApplicationController
   def index
+    @page = "home_page"
   end
 
-  def accommadations
+  def accommodations
+    @page = "accommodations_page"
   end
 
   def weekend
+    @page = "weekend_page"
   end
 
   def shuttle
+    @page = "shuttle_page"
   end
 
   def map
+    @page = "map_page"
   end
 end
