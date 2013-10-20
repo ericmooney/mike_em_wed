c84917753dc2170ebcd8f7df6e8ba865ed394f9b
Eric Mooney <eric.c.mooney@gmail.com>
2013-10-20 16:26:07 -0700

    Add photo to accommodations page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,23 @@ class HomeController < ApplicationController
 
   def accommodations
     @page = "accommodations_page"
+
+    @wedding_location_name = "Gedney Farm"
+    @wedding_address = "34 Hartsville-New Marlborough Road New Marlborough, MA 01230"
+    @accommodations = Accommodation.all
+    @shuttle_accommodations = Accommodation.where(:has_shuttle => true)
+
+    @no_shuttle_accommodations = []
+    @lenox_accommodations = []
+
+    no_shuttle_accommodations = Accommodation.where(:has_shuttle => false)
+    no_shuttle_accommodations.each do |accommodation|
+      if accommodation.address.include?("Lenox")
+        @lenox_accommodations << accommodation
+      else
+        @no_shuttle_accommodations << accommodation
+      end
+    end
   end
 
   def weekend
@@ -33,6 +50,5 @@ class HomeController < ApplicationController
         @no_shuttle_accommodations << accommodation
       end
     end
-    @no_shuttle_accommodations.flatten!
   end
 end
